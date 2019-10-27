dbb757765ce9a9a7921c008944c2a037c4fd5069
cyberay01 <ray.sakanoue@gmail.com>
2019-10-26 19:33:41 -0700

    added adapter for ingredient

---
+++ app/src/main/java/com/example/mymealmaker/IngredientAdapter.java
@@ -0,0 +1,41 @@
+package com.example.mymealmaker;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class IngredientAdapter extends BaseAdapter {
+    Context context;
+    LayoutInflater layoutInflater = null;
+
+    public IngredientAdapter(Context context) {
+        this.context = context;
+        this.layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = layoutInflater.inflate(R.layout.ingredient_row, parent,false);
+
+        ((TextView)convertView.findViewById(R.id.name)).setText(ingredientList.get(position).getName());
+        return convertView;
+    }
+}
